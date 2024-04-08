a8bfb15e54176cfc4efed3029853e6bbc8654d3f
Archilive <victordebournet@gmail.com>
2024-04-08 16:31:38 +0200

    add - colors Global variable

--- src/pages/user/HomePage.tsx
+++ src/pages/user/HomePage.tsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React from 'react';
 
 const HomePage: React.FC = () => {
-    return (
-        <div>
-            <h1>User Home Page</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>User Home Page</h1>
+    </div>
+  );
 };
 
 export default HomePage;
