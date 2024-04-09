1f8cc9774704d40e61efbde0fd84e5d1124d152a
Archilive <victordebournet@gmail.com>
2024-04-09 14:16:44 +0200

    add - add RegisterPage

---
+++ src/components/backgrounds/BackgroundAuth.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const BackgroundAuth = () => {
+  return (
+    <div
+      className="absolute inset-0 my-auto h-[500px]"
+      style={{
+        background:
+          'linear-gradient(152.92deg, rgba(37, 211, 102, 0.2) 4.54%, rgba(7, 193, 96, 0.26) 34.2%, rgba(18, 255, 113, 0.1) 77.55%)',
+        filter: 'blur(118px)',
+      }}
+    />
+  );
+};
+
+export default BackgroundAuth;
