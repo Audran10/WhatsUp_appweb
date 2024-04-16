aa4c246a522a0958411ab9139bfbb180a4dfea5a
ALATORRE <audran.latorre@ynov.com>
2024-04-16 12:29:37 +0200

    start ui converstion

--- src/pages/user/ConversationPage.tsx
+++ src/pages/user/ConversationPage.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
-import Layout from "../../components/user/layout/Layout";
 import { useParams } from "react-router-dom";
 import getConversationById from "../../hooks/conversations/getConversationById";
 import Conversation from "../../models/Conversation";
+import Layout from "../../components/user/layout/Layout";
+import InputMessage from "../../components/user/InputMessage";
 
 const ConversationPage: React.FC = () => {
   const { conversationId } = useParams();
@@ -10,10 +11,12 @@ const ConversationPage: React.FC = () => {
   const [isLoading, setLoading] = useState(true);
 
   useEffect(() => {
-    getConversationById(conversationId ?? "").then((data) => {
-      setConversation(data);
-      setLoading(false);
-    });
+    if (conversationId) {
+      getConversationById(conversationId).then((data) => {
+        setConversation(data);
+        setLoading(false);
+      });
+    }
   }, [conversationId]);
 
   if (isLoading) {
@@ -23,7 +26,10 @@ const ConversationPage: React.FC = () => {
   return (
     <Layout>
       <h1>Hello conversation</h1>
-      <h1 className="text-4xl text-center">{conversation?.name}</h1>
+      <h1 className="flex text-4xl text-center items-end">
+        {conversation?.name}
+      </h1>
+      <InputMessage />
     </Layout>
   );
 };
