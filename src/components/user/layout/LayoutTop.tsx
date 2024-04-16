840974cfd509dec8ee4b7079a7016312580f9134
Archilive <victordebournet@gmail.com>
2024-04-16 12:29:00 +0200

    add - add ProfilePage UI

--- src/components/user/layout/LayoutTop.tsx
+++ src/components/user/layout/LayoutTop.tsx
@@ -1,17 +1,22 @@
-import React from "react";
-import { FaPlus } from "react-icons/fa";
+import React from 'react';
+import { FaPlus } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const LayoutTop: React.FC = () => {
-    return (
-        <div className="flex items-center justify-between w-full h-[8%] bg-secondaryWhite p-3">
-            <button className="cursor-pointer">
-                <img className="w-14 h-14 rounded-full object-center object-cover " src="https://img.freepik.com/photos-gratuite/portrait-lion-genere-par-ia_268835-4278.jpg?size=626&ext=jpg&ga=GA1.1.1518270500.1712448000&semt=ais" alt="logo" />
-            </button>
-            <button className="flex">
-                <FaPlus className="text-mainGray" />
-            </button>
-        </div>
-    );
+  return (
+    <div className="flex items-center justify-between w-full h-[8%] bg-secondaryWhite p-3">
+      <Link to="/profile">
+        <img
+          className="w-14 h-14 rounded-full object-center object-cover "
+          src="https://img.freepik.com/photos-gratuite/portrait-lion-genere-par-ia_268835-4278.jpg?size=626&ext=jpg&ga=GA1.1.1518270500.1712448000&semt=ais"
+          alt="logo"
+        />
+      </Link>
+      <button className="flex">
+        <FaPlus className="text-mainGray" />
+      </button>
+    </div>
+  );
 };
 
 export default LayoutTop;
